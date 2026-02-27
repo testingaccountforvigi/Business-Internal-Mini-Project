3d51448a0800b678edf701e3e654538d4e5c0c24
testingaccountforvigi <rajpurohitmahesh816@gmail.com>
2026-02-27 09:16:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ import numpy as np
 
 model = pickle.load(open("model.pkl", "rb"))
 
-st.title("Google Play Rating vs Revenue Prediction")
-
-st.write("Predict Estimated Revenue Based on App Rating")
+st.title("Google Play Revenue Prediction")
 
 rating = st.slider("App Rating", 1.0, 5.0, 4.0)
+price = st.number_input("App Price ($)", 0.0, 500.0, 0.0)
+installs = st.number_input("Number of Installs", 0, 100000000, 100000)
 
 if st.button("Predict Revenue"):
     
-    input_data = np.array([[rating]])
-    prediction = model.predict(input_data)
-
-    st.success(f"Estimated Revenue: ${prediction[0]:,.2f}")
+    log_installs = np.log1p(installs)
+    input_data = np.array([[rating, price, log_installs]])
+    
+    log_prediction = model.predict(input_data)
+    
+    revenue = np.expm1(log_prediction[0])  # reverse log
+    
+    st.success(f"Estimated Revenue: ${revenue:,.2f}")
